65a9a254698264e8d8df0e20143e75e3d5adac1c
Mohammed Oveze <mdoveze786@gmail.com>
2025-03-03 11:42:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import { getOriginalUrl, shortUrl } from '../Project_Url/Controllers/url.js'
+
+
+
+const app = express();
+
+
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect('mongodb+srv://ovezeov:FwOll9LUSleet6IK@cluster0.y0oga.mongodb.net/',{
+    dbName:"Nodejs"
+}
+).then(()=> console.log('Mongodb connected')).
+catch((error)=> console.log('error'))
+
+
+// rendering ejs file 
+app.get('/',(req,res)=> {
+    res.render('index.ejs' ,{shortUrl : null})
+})
+
+//shorting Url logic
+app.post('/short', shortUrl )
+
+
+//redrict to original url using short code dynamic routing
+app.get('/:shortCode',getOriginalUrl);
+
+
+const port = 2000;
+app.listen(port,()=> console.log(`sever is running on port ${port}`))
